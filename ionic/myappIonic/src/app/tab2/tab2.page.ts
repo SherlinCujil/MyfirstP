2e8ae4aef1214ccc6660f164a5e407db1c66aac7
SherlinCujil <sherlinmc2015@gmail.com>
2023-10-13 23:37:47 -0600

    cambios

--- ionic/myappIonic/src/app/tab2/tab2.page.ts
+++ ionic/myappIonic/src/app/tab2/tab2.page.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable, catchError } from 'rxjs';
-import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
+
 
 @Component({
   selector: 'app-tab2',
@@ -10,51 +9,60 @@ import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 })
 export class Tab2Page {
 
-  loading:boolean =false;
+  loading: boolean = false;
 
-  usuario:any = {};
-  anuncio:any = [];
+  usuario: any = {};
+  anuncios: any = [];
 
-  constructor(private http:HttpClient ) {
-      this.buscarAnuncios();
+  constructor(private http: HttpClient) {
+    this.getAnuncios();
 
-      {
-        const usuarioString = localStorage.getItem("usuario");
-        if (usuarioString !== null) {
-            this.usuario = JSON.parse(usuarioString);
-        } else {
-            location.href = "/tabs/tab1";
-        }
-      
-      }
-
-
+    const usuarioString = localStorage.getItem("usuario");
+    if (usuarioString !== null) {
+      this.usuario = JSON.parse(usuarioString);
+    } else {
+      location.href = "/tabs/tab1";
+    }
 
   }
 
-buscarAnuncios(){
-    this.loading =true;
-    this.buscarAnunciosServicio().subscribe(
-      (response:any)=>this.llenarAnuncios(response)
-    );
+  // buscarAnuncios() {
+  //   this.loading = true;
+  //   this.buscarAnunciosServicio().subscribe(
+  //     (response: any) => this.llenarAnuncios(response)
+  //   );
 
-  }  
+  // }
 
-buscarAnunciosServicio(): Observable<any> {
-  return this.http.get<any>("http>//localhost:8080/anuncio/buscar").pipe(
-    catchError(e=>"error")
-  )
-}
-llenarAnuncios(anuncio:any){
-this.anuncio = anuncio;
-this.loading =false;
-}
+  // buscarAnunciosServicio() {
+  //   var httpOptions = {
+  //     headers: new HttpHeaders({
+  //       'Content-Type': 'application/json'
+  //     })
+  //   }
+  //   return this.http.get<any>("http://localhost:8080/anuncio/buscar", this.anuncios);
+  // }
 
+  // llenarAnuncios(anuncios: any) {
+  //   this.anuncios = anuncios;
+  //   this.loading = false;
+  // }
 
-  
-    logout(){
+  logout() {
     localStorage.removeItem("usuario");
     location.href = "/tabs/tab1";
+  }
+
+  getAnuncios(){
+    this.http.get<any[]>('http://localhost:8080/anuncio/buscar')
+      .subscribe(
+        (data) => {
+          this.anuncios = data;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   }
 
 
